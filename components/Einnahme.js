e761509e3f2028ea0ab933769f3ab83f3449d38c
Tirrego <g.schmittel@gmx.de>
2025-01-10 22:09:17 +0100

    loading animation

--- components/Einnahme.js
+++ components/Einnahme.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 import Button from "./Button";
 
@@ -14,11 +14,11 @@ function Einnahme() {
         const response = await fetch(`/api/user?userId=${userId}`, {
           method: "GET", // Sicherstellen, dass die Methode GET verwendet wird
         });
-  
+
         if (!response.ok) {
           throw new Error(`Fehler: ${response.status}`);
         }
-  
+
         const data = await response.json();
         setGuthaben(data.guthaben); // Guthaben setzen
       } catch (err) {
@@ -27,13 +27,16 @@ function Einnahme() {
         setLoading(false); // Laden abgeschlossen
       }
     }
-  
+
     fetchGuthaben();
   }, []);
-  
 
   if (loading) {
-    return <div className="text-white">Lade Benutzerdaten...</div>;
+    return (
+      <div className="flex justify-center items-center w-screen h-screen bg-gray-900">
+        <div className="w-12 h-12 border-4 border-t-transparent border-white rounded-full animate-spin"></div>
+      </div>
+    );
   }
 
   if (error) {
@@ -43,18 +46,14 @@ function Einnahme() {
   return (
     <div className="flex flex-col justify-center items-center pt-14">
       <h3 className="my-5 text-white">Gesamt</h3>
-      <div className="flex w-48 h-48 rounded-full bg-slate-800/25 justify-center items-center text-3xl text-[#00FF37]">
+      <div
+        className={`flex w-48 h-48 rounded-full bg-slate-800/25 justify-center items-center text-3xl text-[#00FF37] ${
+          guthaben > -1 ? "text-[#00FF37]" : "text-red-500"
+        }`}
+      >
         {guthaben} €
       </div>
       <Button />
-      {/* <h3 className="my-5 text-white">Ausgaben</h3>
-      <div className="flex w-64 h-20 bg-slate-800/25 justify-center items-center text-4xl text-[#DA1009]">
-        0 €
-      </div>
-      <h3 className="my-5 text-white">Einnnahmen</h3>
-      <div className="flex w-64 h-20 bg-slate-800/25 justify-center items-center text-4xl text-[#00FF37]">
-        0 €
-      </div> */}
     </div>
   );
 }
